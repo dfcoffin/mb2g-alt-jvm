ec737694cee37ca905a148229960846ddc672b2d
Donald F. Coffin <(donald.coffin@reminetworks.com)>
2018-12-30 18:42:41 -0500

    1) Adding Groovy Example Branch

---
+++ src/main/groovy/GroovyHello.groovy
@@ -0,0 +1,11 @@
+/**
+ *  Created by Donald F. Coffin on 2018-12-30 at 18:31
+ **/
+class GroovyHello {
+
+    static void main(String[] args) {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld()
+
+        println javaHelloWorld.hello
+    }
+}
